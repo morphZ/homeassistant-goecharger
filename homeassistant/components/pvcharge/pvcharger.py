f4fd40f51b80f5f2cb63753c3deea454f1e7ed45
morphZ <franz.kirchhoff@gmail.com>
2021-11-02 22:18:39 +0000

    Add logic to switch between idle and pv states.

--- homeassistant/components/pvcharge/pvcharger.py
+++ homeassistant/components/pvcharge/pvcharger.py
@@ -6,14 +6,22 @@ import logging
 from typing import Callable
 
 from simple_pid import PID
+from transitions.core import MachineError
 from transitions.extensions.asyncio import AsyncMachine
 
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers.event import async_track_time_interval
+from homeassistant.helpers.event import (
+    async_track_state_change,
+    async_track_time_interval,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
 REFRESH_INTERVAL = timedelta(seconds=5)
+MOVING_AVERAGE_WINDOW = 10
+CONF_GRID_BALANCE_ENTITY = "input_number.grid_return"
+CONF_PV_THRESHOLD = 0.5
+CONF_PV_HYSTERESIS = 10.0
 
 
 class PVCharger:
@@ -21,7 +29,8 @@ class PVCharger:
 
     states = ["off", "idle", "pv", "boost", "calendar"]
     transitions = [
-        ["start", "off", "pv"],
+        ["start", "idle", "pv"],
+        ["pause", "pv", "idle"],
         ["battery_low", "*", "boost"],
         ["battery_ok", "boost", "pv"],
         ["calendar_event", ["off", "pv"], "calendar"],
@@ -39,28 +48,70 @@ class PVCharger:
             states=PVCharger.states,
             transitions=PVCharger.transitions,
             initial="off",
+            queued=True,
         )
 
         self.pid = PID(
             -1.0, -0.1, 0.0, setpoint=0.0, sample_time=1, output_limits=(2.0, 11.0)
         )
         self.control: float | None = 0.0
+        self.current: float | None = None
         self.pid_handle: Callable | None = None
         self.pid_interval: timedelta = REFRESH_INTERVAL
+
+        self.watch_handle = async_track_state_change(
+            self.hass, CONF_GRID_BALANCE_ENTITY, self.async_watch_balance
+        )
+
+    @callback
+    async def async_update_pid(self, event_time) -> None:
+        """Update pid controller values."""
+        _LOGGER.debug("Call async_update_pid() callback at %s", event_time)
+        self.control = self.pid(self.current)  # type: ignore
+        _LOGGER.debug(
+            "Data is self.current=%s, self.control=%s", self.current, self.control
+        )
+
+    def close(self):
+        """Close open callback handles."""
+        _LOGGER.debug("Closing %s", self)
+        if self.watch_handle is not None:
+            self.watch_handle()
+            self.pid_handle = None
+
+    @callback
+    async def async_watch_balance(self, entity, old_state, new_state) -> None:
+        """Watch for grid balance and act accordingly."""
+        _LOGGER.debug("Enter async_watch_balance callback.")
+
+        self.current = float(new_state.state)
+
+        if self.current > CONF_PV_THRESHOLD:
+            try:
+                await self.start()  # type: ignore
+            except MachineError as exc:
+                _LOGGER.debug(
+                    "Tried to switch to start, but MachineError occurred",
+                    exc_info=exc,
+                )
+
+        if self.current < CONF_PV_THRESHOLD - (
+            CONF_PV_HYSTERESIS if self.is_pv() else 0  # type: ignore
+        ):
+            try:
+                await self.pause()  # type: ignore
+            except MachineError as exc:
+                _LOGGER.debug(
+                    "Tried to switch to pause, but MachineError occurred",
+                    exc_info=exc,
+                )
 
     async def on_enter_pv(self) -> None:
         """Start control loop for pv controlled charging."""
 
-        @callback
-        async def update_pid(event_time) -> None:
-            _LOGGER.info("Call update_pid() callback at %s", event_time)
-            current = float(self.hass.states.get("input_number.grid_return").state)  # type: ignore
-            self.control = self.pid(current)
-            _LOGGER.info("Data is current=%s, self.control=%s", current, self.control)
-
         self.pid_handle = async_track_time_interval(
             self.hass,
-            update_pid,
+            self.async_update_pid,
             self.pid_interval,
         )
 
