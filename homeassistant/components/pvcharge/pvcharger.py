0a090aa89498102e955a5a05ad024fab35818847
morphZ <franz.kirchhoff@gmail.com>
2021-11-03 12:24:47 +0000

    Implement boost mode with timeout.

--- homeassistant/components/pvcharge/pvcharger.py
+++ homeassistant/components/pvcharge/pvcharger.py
@@ -10,6 +10,7 @@ from transitions.extensions.asyncio import AsyncMachine
 
 from homeassistant.core import HomeAssistant, callback
 from homeassistant.helpers.event import (
+    async_call_later,
     async_track_state_change,
     async_track_time_interval,
 )
@@ -21,21 +22,20 @@ MOVING_AVERAGE_WINDOW = 10
 CONF_GRID_BALANCE_ENTITY = "input_number.grid_return"
 CONF_PV_THRESHOLD = 0.5
 CONF_PV_HYSTERESIS = 10.0
+CONF_DEFAULT_MAX_TIME = timedelta(minutes=30)
 
 
 class PVCharger:
     # pylint: disable=no-member
     """Finite state machine to control the PV charging."""
 
-    states = ["off", "idle", "pv", "boost", "calendar"]
+    states = ["off", "idle", "pv", "max", "low", "calendar"]
     transitions = [
+        ["auto", ["off", "max", "low", "calendar"], "pv", "enough_power"],
+        ["auto", ["off", "max", "low", "calendar"], "idle"],
         ["start", "idle", "pv"],
         ["pause", "pv", "idle"],
-        ["battery_low", "*", "boost"],
-        ["battery_ok", "boost", "pv"],
-        ["calendar_event", ["off", "pv"], "calendar"],
-        ["touch", ["pv", "calendar"], "="],
-        ["off", "*", "off"],
+        ["boost", "*", "max"],
     ]
 
     def __init__(self, hass: HomeAssistant) -> None:
@@ -58,9 +58,10 @@ class PVCharger:
         self.current = float(hass.states.get(CONF_GRID_BALANCE_ENTITY).state)  # type: ignore
         self.pid_interval = REFRESH_INTERVAL
         self.pid_handle: Callable | None = None
+        self.timeisup_handle: Callable | None = None
 
         self.watch_handle = async_track_state_change(
-            self.hass, CONF_GRID_BALANCE_ENTITY, self.async_watch_balance
+            self.hass, CONF_GRID_BALANCE_ENTITY, self._async_watch_balance
         )
 
     async def go(self) -> None:
@@ -71,9 +72,9 @@ class PVCharger:
             await self.to_idle()  # type: ignore
 
     @callback
-    async def async_update_pid(self, event_time) -> None:
+    async def _async_update_pid(self, event_time) -> None:
         """Update pid controller values."""
-        _LOGGER.debug("Call async_update_pid() callback at %s", event_time)
+        _LOGGER.debug("Call _async_update_pid() callback at %s", event_time)
         self.control = self.pid(self.current)  # type: ignore
         _LOGGER.debug(
             "Data is self.current=%s, self.control=%s", self.current, self.control
@@ -90,9 +91,9 @@ class PVCharger:
         return self.current > threshold
 
     @callback
-    async def async_watch_balance(self, entity, old_state, new_state) -> None:
+    async def _async_watch_balance(self, entity, old_state, new_state) -> None:
         """Watch for grid balance and act accordingly."""
-        _LOGGER.debug("Enter async_watch_balance callback.")
+        _LOGGER.debug("Enter async_watch_balance callback")
 
         self.current = float(new_state.state)
 
@@ -102,12 +103,12 @@ class PVCharger:
         if self.is_idle() and self.enough_power:  # type: ignore
             await self.start()  # type: ignore
 
-    async def on_enter_pv(self) -> None:
+    async def on_enter_pv(self, *args, **kwargs) -> None:
         """Start control loop for pv controlled charging."""
 
         self.pid_handle = async_track_time_interval(
             self.hass,
-            self.async_update_pid,
+            self._async_update_pid,
             self.pid_interval,
         )
 
@@ -117,6 +118,25 @@ class PVCharger:
         if self.pid_handle is not None:
             self.pid_handle()
             self.pid_handle = None
+
+    @callback
+    async def _async_time_is_up(self, *args, **kwargs) -> None:
+        self.timeisup_handle = None
+        await self.auto()  # type: ignore
+
+    async def on_enter_max(self, duration: timedelta = CONF_DEFAULT_MAX_TIME) -> None:
+        """Load EV battery with maximal power."""
+
+        self.timeisup_handle = async_call_later(
+            self.hass, duration, self._async_time_is_up
+        )
+
+    async def on_exit_max(self) -> None:
+        """Cancel pending timeouts."""
+
+        if self.timeisup_handle is not None:
+            self.timeisup_handle()
+            self.timeisup_handle = None
 
     def close(self):
         """Close open callback handles."""
